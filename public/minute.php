76b9096bf6bec53566a8922f1a3450efe03df3c5
Christophe HERVI <christophe.hervi@free.fr>
2024-08-20 20:41:16 +0200

    les zone erreur + réponse chaine de caractère vide sont validées

--- public/minute.php
+++ public/minute.php
@@ -6,5 +6,7 @@
  * @return string|bool le texte pour la partie heure ou false si une erreur est survenue
  **/
 function Minute(int $heure, int $minute) : string|bool {
-  return false; #
+  if (($heure < 0) || ($heure > 23) || ($minute < 0) || ($minute > 59)) return false;
+  if ($minute == 0) return '';
+  return "$minute";
 }
