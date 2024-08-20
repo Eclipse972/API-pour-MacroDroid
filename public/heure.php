a0aafb24f7bd11a2d50b9c6e117602f24c66abb6
Christophe HERVI <christophe.hervi@gmail.com>
2024-08-20 13:26:49 +0200

    Update heure.php
    création de la fonction qui ne fait rien pour le moment

--- public/heure.php
+++ public/heure.php
@@ -1,1 +1,9 @@
 <?php
+/**
+ * renvoie le texte correpondant à la partie heure
+ * @param int heure
+ * @return string|bool le texte pour la partie heure ou false si une erreur est survenue
+ **/
+function Heure(int $valeur) : string|bool {
+  return false;
+}
