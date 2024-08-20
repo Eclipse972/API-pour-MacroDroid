86a703f18ab40987be263b1fb2fb2ab0e0f83087
Christophe HERVI <christophe.hervi@gmail.com>
2024-08-20 13:37:05 +0200

    erreur sur le nom du paramètre

--- public/heure.php
+++ public/heure.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * renvoie le texte correpondant à la partie heure
+ * renvoie le texte correspondant à la partie heure
  * @param int heure
  * @return string|bool le texte pour la partie heure ou false si une erreur est survenue
  **/
-function Heure(int $valeur) : string|bool {
+function Heure(int $heure) : string|bool {
   return false;
 }
