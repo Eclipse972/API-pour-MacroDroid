75a9a5a337ec37e357b590218230a67265f9bb29
Christophe HERVI <christophe.hervi@free.fr>
2024-08-21 00:18:11 +0200

    test réussi

--- public/minute.php
+++ public/minute.php
@@ -6,7 +6,9 @@
  * @return string|bool le texte pour la partie heure ou false si une erreur est survenue
  **/
 function Minute(int $heure, int $minute) : string|bool {
-  if (($heure < 0) || ($heure > 23) || ($minute < 0) || ($minute > 59)) return false;
-  if ($minute == 0) return '';
-  return "$minute";
+	if (($heure < 0) || ($heure > 23) || ($minute < 0) || ($minute > 59)) return false;
+	if ($minute == 0) return '';
+	if (($minute > 12) && ($minute < 18) && ($heure < 13)) return 'et quart';
+	if (($minute > 27) && ($minute < 33) && ($heure < 13)) return 'et demi';
+	return "$minute";
 }
