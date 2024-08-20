dfcad833b950c3542499c39ca6a7af509a9c5058
Christophe HERVI <christophe.hervi@gmail.com>
2024-08-20 13:30:26 +0200

    Update minute.php
    Création de la fonction qui ne fait rien pour le moment

--- public/minute.php
+++ public/minute.php
@@ -1,1 +1,10 @@
 <?php
+/**
+ * renvoie le texte correspondant à la partie minute
+ * @param int heure
+ * @param int minute
+ * @return string|bool le texte pour la partie heure ou false si une erreur est survenue
+ **/
+function Minute(int $heure, int $minute) : string|bool {
+  return false;
+}
