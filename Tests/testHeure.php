c7bbb8a0926129326933e586d32aa8b495fbaf5b
Christophe HERVI <christophe.hervi@free.fr>
2024-08-20 15:02:10 +0200

    rédaction du test pour l'heure

--- Tests/testHeure.php
+++ Tests/testHeure.php
@@ -1,40 +1,42 @@
 <?php
-include '../public/heure.php';
-
-# les assertions à vérifier
-$assertions = array(
-  0 => 'minuit',
-  1 => '1 heure',
-  2 => '2 heure', # l'orthographe n'a pas d'importance il faut quelque chose de prononcable par la synthèse vocale
-  3 => '3 heure',
-  4 => '4 heure',
-  5 => '5 heure',
-  6 => '6 heure',
-  7 => '7 heure',
-  8 => '8 heure',
-  9 => '9 heure',
-  10 => '10 heure',
-  11 => '11 heure',
-  12 => 'midi',
-  13 => '13 heure',
-  14 => '14 heure',
-  15 => '15 heure',
-  16 => '16 heure',
-  17 => '17 heure',
-  18 => '18 heure',
-  19 => '19 heure',
-  20 => '20 heure',
-  21 => '21 heure',
-  22 => '22 heure',
-  23 => '23 heure',
-);
-
+include 'public/heure.php';
 function testHeure() : string {
-  for($heure = 0; $heure < 24; $heure++)
-    if (($reponse = Heure($heure)) != $assertion[$heure] {
-      return "Réponse attendue pour $heure = {$assertion[$heure]} réponse donnée $reponse";
-  
-  # test valeur hors intervalle
-  if ((Heure(-1) != false) || (Heure(26) != false)) return "valeurs hors intervalle non détectée";
-  return "test Heure() réussi";
+	# les assertions à vérifier
+	$assertion = array(
+		0 => 'minuit',
+		1 => '1 heure',
+		2 => '2 heure', # l'orthographe n'a pas d'importance il faut quelque chose de prononcable par la synthèse vocale
+		3 => '3 heure',
+		4 => '4 heure',
+		5 => '5 heure',
+		6 => '6 heure',
+		7 => '7 heure',
+		8 => '8 heure',
+		9 => '9 heure',
+		10 => '10 heure',
+		11 => '11 heure',
+		12 => 'midi',
+		13 => '13 heure',
+		14 => '14 heure',
+		15 => '15 heure',
+		16 => '16 heure',
+		17 => '17 heure',
+		18 => '18 heure',
+		19 => '19 heure',
+		20 => '20 heure',
+		21 => '21 heure',
+		22 => '22 heure',
+		23 => '23 heure',
+	);
+
+	for($heure = 0; $heure < 24; $heure++)
+	if (($reponse = Heure($heure)) != $assertion[$heure])
+		return "Réponse attendue pour $heure = {$assertion[$heure]} réponse donnée $reponse";
+
+	# test valeur hors intervalle
+	if ((Heure(-1) != false) || (Heure(26) != false))
+		return "valeurs hors intervalle non détectée";
+	return "test Heure() réussi";
 }
+
+echo testHeure(), "\n";
