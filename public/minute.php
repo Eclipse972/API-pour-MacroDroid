6823898de833bf6ceed9a5d6068068b882dfd37a
Christophe HERVI <christophe.hervi@free.fr>
2024-08-20 19:08:30 +0200

    le test échoue

--- public/minute.php
+++ public/minute.php
@@ -6,5 +6,5 @@
  * @return string|bool le texte pour la partie heure ou false si une erreur est survenue
  **/
 function Minute(int $heure, int $minute) : string|bool {
-  return false;
+  return false; #
 }
