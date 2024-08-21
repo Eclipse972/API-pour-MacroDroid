b77709132dc33a4c4c6804a9e65c2d14a4deb08b
Christophe HERVI <christophe.hervi@free.fr>
2024-08-21 11:45:38 +0200

    test réussi réponse formatée = { 	"year":2024, 	"month":8, 	"day":21, 	"hour":11, 	"minute":44, 	"seconds":2, 	"milliSeconds":48, 	"dateTime":"2024-08-21T11:44:02.0480455", 	"date":"08/21/2024", 	"time":"11:44", 	"timeZone":"Europe/Paris", 	"dayOfWeek":"Wednesday", 	"dstActive":true }

---
+++ Tests/testRequete.php
@@ -0,0 +1,28 @@
+<?php
+# requête vers timeAPI
+$ch = curl_init();
+
+$url = "https://www.timeapi.io/api/time/current/coordinate?latitude=49&longitude=2";
+
+// Configuration des options cURL
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Optionnel : Spécifier que vous attendez du JSON dans la réponse
+$headers = [
+    'Accept: application/json',
+];
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+// Exécution de la requête
+$response = curl_exec($ch);
+
+// Gestion des erreurs
+if (curl_errno($ch)) {
+    echo 'Erreur cURL : ' . curl_error($ch);
+} else {
+    echo $response;
+}
+
+// Fermeture de la session cURL
+curl_close($ch);
