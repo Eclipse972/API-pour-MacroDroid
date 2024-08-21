cdca072b246f8d16b24a192f0ef9a806895c6966
Christophe HERVI <christophe.hervi@free.fr>
2024-08-21 14:34:51 +0200

    ébauche construction de la réponse

--- public/index.php
+++ public/index.php
@@ -5,9 +5,27 @@ include 'extractionParametre.php';
 
 # récupération des paramètres de type float
 $latitude = extractionParamètre('latitude', $_GET);
-$longitude = extractionParamètre('longitude', $_GET);
- 
-if ((!$latitude) || (!$longitude)) { ?>
-{ "erreur" : "paramètres incorrects"}
-<?php exit;
+if (!$latitude) {
+	echo "{ \"erreur\" : \"latitude incorrecte\"}";
+	exit;
 }
+
+$longitude = extractionParamètre('longitude', $_GET);
+if (!$longitude) {
+	echo "{ \"erreur\" : \"longitude incorrecte\"}";
+	exit;
+}
+
+$reponse = requeteAPI($latitude, $longitude);
+
+if (is_string($reponse)) {
+	echo "{\"erreur\":\"$reponse\"}";
+	exit;
+}
+
+if ((is_array($reponse)) && (isset($reponse['heure'])) && (isset($reponse['minute']))) {
+	echo "{\"heure\":\"{$reponse['heure']}\", \"minute\":\"{$reponse['minute']}\"}";
+	exit;
+}
+
+echo "{ \"erreur\" : \"inconnue\"}";
