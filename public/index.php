ef24c4bb54f15904c26263f0fa167c025247bba0
Christophe HERVI <christophe.hervi@free.fr>
2024-08-21 14:51:51 +0200

    remplacement echo par code JSON direct

--- public/index.php
+++ public/index.php
@@ -7,26 +7,34 @@ include 'requete.php';
 # récupération des paramètres de type float
 $latitude = extractionParamètre('latitude', $_GET);
 if (!$latitude) {
-	echo "{ \"erreur\" : \"latitude incorrecte\"}";
+?>
+{"erreur":"latitude incorrecte"}
+<?php
 	exit;
 }
 
 $longitude = extractionParamètre('longitude', $_GET);
 if (!$longitude) {
-	echo "{ \"erreur\" : \"longitude incorrecte\"}";
+?>
+{"erreur":"longitude incorrecte"}
+<?php
 	exit;
 }
 
 $reponse = requeteAPI($latitude, $longitude);
 
 if (is_string($reponse)) {
-	echo "{\"erreur\":\"$reponse\"}";
+?>
+{"erreur":"<?=$reponse?>"}
+<?php
 	exit;
 }
 
 if ((is_array($reponse)) && (isset($reponse['heure'])) && (isset($reponse['minute']))) {
-	echo "{\"heure\":\"{$reponse['heure']}\", \"minute\":\"{$reponse['minute']}\"}";
+?>
+{"heure":"<?=$reponse['heure']?>","minute":"<?=$reponse['minute']?>}
+<?php
 	exit;
 }
-
-echo "{ \"erreur\" : \"inconnue\"}";
+?>
+{"erreur":"inconnue"}
