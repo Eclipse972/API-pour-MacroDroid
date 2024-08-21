59b2ad5508fbc943beb8b5e26097cb1d27dae3a2
Christophe HERVI <christophe.hervi@free.fr>
2024-08-21 14:11:53 +0200

    erreur de variable

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ include 'extractionParametre.php';
 
 # récupération des paramètres de type float
 $latitude = extractionParamètre('latitude', $_GET);
-$latitude = extractionParamètre('longitude', $_GET);
+$longitude = extractionParamètre('longitude', $_GET);
  
 if ((!$latitude) || (!$longitude)) { ?>
 { "erreur" : "paramètres incorrects"}
