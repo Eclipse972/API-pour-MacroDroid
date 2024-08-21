b7584ce43294c3b16c0b0807f466219593ea018f
Christophe HERVI <christophe.hervi@free.fr>
2024-08-21 13:12:10 +0200

    création de requeteAPI et son premier test

---
+++ public/requete.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * renvoie l'heure locale pour des coordonées GPS données
+ * @param float latitude
+ * @param float longitude
+ * @return array|string tableau associatif contenant heure et minute ou un message d'erreur
+ */
+function requeteAPI(float $latitude = 49.4295387, float $longitude = 2.0807123) : array|string {
+	$requeteCURL = curl_init();
+	curl_setopt(
+		$requeteCURL,
+		CURLOPT_URL,
+		"https://www.timeapi.io/api/time/current/coordinate?latitude=$latitude&longitude=$longitude" # requête vers timeAPI
+	);
+	curl_setopt($requeteCURL, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($requeteCURL, CURLOPT_HTTPHEADER, ['Accept: application/json']);
+	$reponse = curl_exec($requeteCURL);
+
+	// Gestion des erreurs
+	if (curl_errno($requeteCURL)) {
+		$message = 'Erreur cURL : ' . curl_error($requeteCURL);
+	} else {
+		$data = json_decode($reponse, true);
+		$message = false;
+	}
+	curl_close($requeteCURL);
+	return !$message ? # si pas de message ?
+			array('heure' => intval($data['hour']), 'minute'=> intval($data['minute'])) :
+			$message;
+}
