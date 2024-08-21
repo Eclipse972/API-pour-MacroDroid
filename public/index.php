1c6994ae2b5aeba1e2e95bfb9d4e9b750cd6d607
Christophe HERVI <christophe.hervi@free.fr>
2024-08-21 11:30:14 +0200

    utilisation de extractionParamètre()

--- public/index.php
+++ public/index.php
@@ -1,1 +1,13 @@
 <?php
+include 'minute.php';
+include 'heure.php';
+include 'extractionParametre.php';
+
+# récupération des paramètres de type float
+$latitude = extractionParamètre('latitude', $_GET);
+$latitude = extractionParamètre('longitude', $_GET);
+ 
+if ((!$latitude) || (!$longitude)) { ?>
+{ "erreur" : "paramètres incorrects"}
+<?php exit;
+}
